fa04375b9bd4ec6eb37fdf48537efef8aaa6f5d4
Leonardo <leo@radbitt.com>
2016-04-06 15:38:55 -0700

    Added pages
    Also, added routes to the new pages.

--- app/app.py
+++ app/app.py
@@ -6,9 +6,25 @@ app = Flask(__name__)
 db = events.initialize()
 
 @app.route('/')
-def index():
+def index(tile='Home'):
 	timelineJSON = events.jsonTable()
 	return render_template("index.html", timelineJSON=timelineJSON)
+
+@app.route('/about')
+def about(title='About Us'):
+	return render_template("about.html", title=title )
+
+@app.route('/service')
+def service(title='Service'):
+	return render_template("service.html")
+
+@app.route('/information')
+def information(title=''):
+	return render_template("information.html")
+
+@app.route('/getinvolved')
+def getInvolved(title=''):
+	return render_template("getinvolved.html")
 
 if __name__ == '__main__':
 	#App can be rendered at -> localhost:5000 or 127.0.0.1:5000
